2d8db26070b740fd4f4b8dd4bafd478fa5315de2
Gola-k <kunalgola426@gmail.com>
2022-10-01 15:17:52 +0530

    1st commit

---
+++ app/src/main/java/com/example/firstproject2kotlin/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.firstproject2kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.firstproject2kotlin.ui.theme.Firstproject2KotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DefaultPreview()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column {
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = "JetPack Compose Tutorial",
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Left,
+        )
+        Text(
+            text = "Jetpack Compose is a modern toolkit for building native Android UL Compose simplifies and accelerates Ul development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify,
+        )
+        Text(
+            text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Firstproject2KotlinTheme {
+        Greeting("Android")
+    }
+}
